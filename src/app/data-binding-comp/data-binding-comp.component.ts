32a1065b69e4aa55e9d7f0040a00e15565784da5
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-27 00:13:13 +0530

    property binding

--- src/app/data-binding-comp/data-binding-comp.component.ts
+++ src/app/data-binding-comp/data-binding-comp.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-binding-comp.component.css']
 })
 export class DataBindingCompComponent {
- 
+  data = "Welcome to Property binding"
 
 }
