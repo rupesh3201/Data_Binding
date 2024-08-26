afbf3ba8685f26ef2c774f894d1a9d46a4b97279
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-27 00:42:40 +0530

    property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataBindingCompComponent } from './data-binding-comp/data-binding-comp.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingCompComponent
+    DatabindingComponent,
+  
   ],
   imports: [
     BrowserModule,
