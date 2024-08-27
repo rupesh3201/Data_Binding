e6ecfc06ee54130240e8cb64757eaec11f112106
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-28 01:14:32 +0530

    mouseover function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,9 +10,20 @@ export class AppComponent {
   title = 'DataBinding';
   str = "Rupesh comes form ts file  and  below img link also";
   imgpath = "assets/rupesh.JPG"
+  res =0
+  result =0
   fun()
   {
     this.str = "thanks you we send  Rupesh related information";
     console.log("data submitted sucessfully")
+  }
+  
+  add( num1:any, num2:any)
+  {
+    
+    
+    this.res = parseInt(num1)+parseInt(num2);
+    console.log(this.res);
+    
   }
 }
