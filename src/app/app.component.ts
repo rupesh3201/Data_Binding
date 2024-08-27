252ab5622eb109e85603346cd7872de6ffe9998e
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-28 01:34:00 +0530

    addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { NgIf } from '@angular/common';
+import { NONE_TYPE } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,20 +11,31 @@ export class AppComponent {
   title = 'DataBinding';
   str = "Rupesh comes form ts file  and  below img link also";
   imgpath = "assets/rupesh.JPG"
-  res =0
-  result =0
+   res:any 
+  result: any 
   fun()
   {
     this.str = "thanks you we send  Rupesh related information";
     console.log("data submitted sucessfully")
   }
-  
+  // addtion
   add( num1:any, num2:any)
   {
     
     
     this.res = parseInt(num1)+parseInt(num2);
     console.log(this.res);
+  }
+  mul( num1:any, num2:any)
+  {
     
+    
+    this.res = parseInt(num1)-parseInt(num2);
+    console.log(this.res);
+  }
+ 
+  out()
+  {
+    this.res = "" ;
   }
 }
